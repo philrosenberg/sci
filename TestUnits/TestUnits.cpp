b5d418fc22e1596b0a6906415b6e2fb56b0a3e40
Phil Rosenberg <p.d.rosenberg@gmail.com>
2019-11-15 14:45:59 +0000

    Added some quick code to test Units.
    This should be expanded further, but at least it checks some
    basic compilation for now.

---
+++ TestUnits/TestUnits.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include"../include/svector/Units.h"
+#include<assert.h>
+
+typedef sci::Physical<sci::Metre<>, double> dMetre;
+typedef sci::Physical<sci::Second<>, double> dSecond;
+typedef sci::Physical<sci::Metre<2>, double> dMetreSquared;
+typedef sci::Physical<sci::Second<2>, double> dSecondSquared;
+typedef sci::Physical<sci::Metre<1,6>, double> dMegaMetre;
+typedef sci::Physical<sci::Second<1,-9>, double> dnanoSecond;
+
+int main()
+{
+	dMetre length1(1.0);
+	dMegaMetre length2(0.1);
+	dMetre length3 = length1 + length2;
+	dMetre expectedSumLengthMetre(100001.0);
+	dMegaMetre expectedSumLengthMegaMetre(0.100001);
+	assert(std::abs(length3.value<dMetre>() - expectedSumLengthMetre.value<dMetre>()) < 0.001);
+	assert(std::abs(length3.value<dMegaMetre>() - expectedSumLengthMetre.value<dMegaMetre>()) < 0.001);
+	assert(std::abs(length3.value<dMetre>() - expectedSumLengthMegaMetre.value<dMetre>()) < 0.001);
+	assert(std::abs(length3.value<dMegaMetre>() - expectedSumLengthMegaMetre.value<dMegaMetre>()) < 0.001);
+
+	auto speed = dMetre(200.0) / dSecond(50.0);
+	assert(std::abs(speed.value<decltype(speed)>() - 4.0) < 0.001);
+}
+
