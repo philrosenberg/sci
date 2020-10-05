3dee34a290201ff2ae6ea8945380150aa56bd074
Phil Rosenberg <p.d.rosenberg@gmail.com>
2020-10-05 09:54:05 +0100

    Fixed normalisation for fft

--- include/svector/dep/svector.h
+++ include/svector/dep/svector.h
@@ -2250,8 +2250,8 @@ namespace sci
 	void resample(const std::vector<double> &input, double factor, std::vector<double> &output);
 	void resample(const std::vector<float> &input, float factor, std::vector<float> &output);
 	//fft
-	void fft(const std::vector<double> &re_input, std::vector<double> &re_output, std::vector<double> &im_output);
-	void ifft(const std::vector<double> &re_input, std::vector<double> &im_input, std::vector<double> &re_output);
+	void fft(const std::vector<double> &re_input, std::vector<double> &re_output, std::vector<double> &im_output, bool normaliseForNumberOfPoints = true, bool performNegativeFrequencyAmplitudedoubling = true);
+	void ifft(const std::vector<double> &re_input, std::vector<double> &im_input, std::vector<double> &re_output, bool undoNormaliseForNumberOfPoints = false, bool performNegativeFrequencyAmplitudedoubling = true);
 	//discrete power spectrum normalised so that the sum of the output is equal to the variance of the input
 	std::vector<double> powerspectrum(const std::vector<double> &v);
 	//discrete cospectrum normalised so that the sum of the output is equal to the covariance of the inputs
