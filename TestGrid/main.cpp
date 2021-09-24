e9119db4049c345f25a0eb84932de08c2ac9c29a
Phil Rosenberg <p.d.rosenberg@gmail.com>
2021-09-24 16:00:49 +0100

    Finished the grid_view class

--- TestGrid/main.cpp
+++ TestGrid/main.cpp
@@ -1,13 +1,73 @@
 #include"../include/svector/array.h"
 #include<iostream>
 
+template<class BASERANGE>
+void output2d(sci::grid_view<2, BASERANGE> grid)
+{
+	std::array<size_t, 2> shape = grid.shape();
+
+	for (size_t i = 0; i < shape[0]; ++i)
+	{
+		for (size_t j = 0; j < shape[1]; ++j)
+			std::cout << grid[i][j] << ",";
+		std::cout << "\n";
+	}
+	std::cout << "\n\n";
+}
+
+template<class BASERANGE>
+void output1d(sci::grid_view<1, BASERANGE> grid)
+{
+	std::array<size_t, 1> shape = grid.shape();
+
+	for (size_t i = 0; i < shape[0]; ++i)
+	{
+			std::cout << grid[i] << ",";
+	}
+	std::cout << "\n\n";
+}
+
+
 int main()
 {
-	std::views::transform;
+	std::array<size_t, 2>shapeArray{ 3, 2 };
+	std::array<size_t, 1>stridesArray{2};
+	sci::GridPremultipliedStridesReference<2> strides2d(&stridesArray[0]);
 
+	//sci::GridPremultipliedStridesReference<1> strides1d;
+
+	//start with some data
 	std::deque<double> que{ 1., 2., 3., 4., 5., 6. };
-	auto gridRange = que | sci::views::grid;
-	for (size_t i = 0; i < gridRange.size(); ++i)
-		std::cout << gridRange[i] << ",";
+	auto subrange = std::ranges::subrange(que.begin() + 2, que.begin()+5);
+
+	//create a 1D grid from que and output it
+	auto gridRange1d1 = que | sci::views::grid<1>;
+	for (size_t i = 0; i < gridRange1d1.size(); ++i)
+		std::cout << gridRange1d1[i] << ",";
 	std::cout << "\n\n";
+
+	//create a 1D grid from subrange and output it
+	auto gridRange1d2 = subrange | sci::views::grid<1>;
+	for (size_t i = 0; i < gridRange1d2.size(); ++i)
+		std::cout << gridRange1d2[i] << ",";
+	std::cout << "\n\n";
+
+	//create a 2D grid from que and output it
+	auto gridRange = que | sci::views::grid<2>(strides2d);
+	//auto gridRange = que | sci::grid_fn<2>(shape1);
+	//for (size_t i = 0; i < gridRange.size(); ++i)
+	//	std::cout << gridRange[i] << ",";
+	for (size_t i = 0; i < shapeArray[0]; ++i)
+	{
+		for (size_t j = 0; j < shapeArray[1]; ++j)
+			std::cout << gridRange[i][j] << ",";
+		std::cout << "\n";
+	}
+	std::cout << "\n\n";
+
+	//pass the grid into a function expecting a 2d grid
+	output2d(gridRange);
+	output1d(gridRange1d1);
+
+	return 0;
 }
--- include/svector/array.h
+++ include/svector/array.h
@@ -1162,9 +1162,98 @@ namespace sci
 		std::span<T> m_span;
 	};
 
+	template<size_t NDIMS>
+	class GridPremultipliedStridesPointer
+	{
+	public:
+		GridPremultipliedStridesPointer(const size_t* ptrIn)
+			:ptr(ptrIn)
+		{}
+		GridPremultipliedStridesPointer() = default;
+		const size_t* ptr;
+	};
+	template<>
+	class GridPremultipliedStridesPointer<1>
+	{
+	public:
+		GridPremultipliedStridesPointer() = default;
+	};
+	template<>
+	class GridPremultipliedStridesPointer<0>
+	{
+	public:
+		GridPremultipliedStridesPointer() = default;
+	};
+
+	template<size_t NDIMS>
+	class GridPremultipliedStridesReference
+	{
+	public:
+		GridPremultipliedStridesReference() = default;
+		GridPremultipliedStridesReference(const size_t* stridesPtr) requires (NDIMS > 1)
+			:m_ptr(stridesPtr)
+		{
+		}
+		const size_t stride() const requires (NDIMS > 1)
+		{
+			return *(m_ptr.ptr);
+		}
+		const size_t stride() const requires (NDIMS == 1)
+		{
+			return 1;
+		}
+		const size_t stride() const requires (NDIMS == 0)
+		{
+			return 0;
+		}
+		GridPremultipliedStridesReference<NDIMS - 1> next() const requires (NDIMS > 2)
+		{
+			return GridPremultipliedStridesReference<NDIMS - 1>(m_ptr.ptr + 1);
+		}
+		GridPremultipliedStridesReference<NDIMS - 1> next() const requires (NDIMS == 1 || NDIMS == 2)
+		{
+			return GridPremultipliedStridesReference<NDIMS - 1>();
+		}
+		std::array<size_t, NDIMS> getShape(size_t size) const
+		{
+			if constexpr (NDIMS == 0)
+			{
+				return std::array<size_t, 0>{};
+			}
+			else if constexpr (NDIMS == 1)
+			{
+			return std::array<size_t, 1>{size};
+			}
+			else
+			{
+				std::array<size_t, NDIMS> result;
+				result[0] = size / m_ptr.ptr[0];
+				for (size_t i = 1; i < NDIMS - 1; ++i)
+					result[i] = m_ptr.ptr[i - 1] / m_ptr.ptr[i];
+				result[NDIMS - 1] = m_ptr.ptr[NDIMS - 2];
+				return result;
+			}
+		}
+		size_t getOffset(const std::array<size_t, NDIMS>& index)
+		{
+			if constexpr (NDIMS == 0)
+				return 0;
+			else if constexpr (NDIMS == 1)
+				return index;
+			else
+			{
+				size_t result = 0;
+				for (size_t i = 0; i < NDIMS - 1; ++i)
+					result += index * m_ptr.ptr[i];
+				return result + index[NDIMS - 1];
+			}
+		}
+	private:
+		GridPremultipliedStridesPointer<NDIMS> m_ptr;
+	};
 
 
-	template<class RANGE>
+	template<size_t NDIMS, class RANGE>
 	requires std::ranges::random_access_range<RANGE>
 		class grid_view : public std::ranges::view_base
 	{
@@ -1313,8 +1402,8 @@ namespace sci
 		constexpr grid_view& operator=(grid_view&& rhs) = default;
 		~grid_view() = default;
 
-		grid_view(RANGE&& range)
-			 : m_dataMembers{ new data_members_t{std::forward<RANGE>(range)} }
+		grid_view(RANGE&& range, const GridPremultipliedStridesReference<NDIMS> &strides)
+			: m_dataMembers{ new data_members_t{std::forward<RANGE>(range)} }, m_strides(strides)
 		{
 		}
 
@@ -1338,13 +1427,39 @@ namespace sci
 		{
 			return std::size(m_dataMembers->m_range);
 		}
-		reference_type operator[](const difference_type &index)
+		reference_type operator[](const difference_type &index) requires(NDIMS==1)
 		{
 			return m_dataMembers->m_range[index];
 		}
-		const reference_type operator[](const difference_type& index) const
+		const reference_type operator[](const difference_type& index) const requires(NDIMS == 1)
 		{
 			return m_dataMembers->m_range[index];
+		}
+		reference_type operator[](const std::array<size_t, NDIMS> &index)
+		{
+			return m_dataMembers->m_range[m_strides.getOffset(index)];
+		}
+		const reference_type operator[](const std::array<size_t, NDIMS>& index) const
+		{
+			return m_dataMembers->m_range[m_strides.getOffset(index)];
+		}
+		auto operator[](const difference_type& index) requires(NDIMS > 1)
+		{
+			auto subrange = std::ranges::subrange(m_dataMembers->m_range.begin() + index * m_strides.stride(),
+				m_dataMembers->m_range.begin() + (index + 1) * m_strides.stride());
+			static_assert((bool)std::ranges::random_access_range<decltype(subrange)>, "subrange failed the test for being a random access range");
+			return grid_view<NDIMS - 1, decltype(subrange)>(std::forward<decltype(subrange)>(subrange), m_strides.next());
+		}
+		const auto operator[](const difference_type& index) const requires(NDIMS > 1)
+		{
+			auto subrange = std::ranges::subrange(m_dataMembers->m_range.begin() + index * m_strides.stride(),
+				m_dataMembers->m_range.begin() + (index + 1) * m_strides.stride());
+			static_assert((bool)std::ranges::random_access_range<decltype(subrange)>, "subrange failed the test for being a random access range");
+			return grid_view<NDIMS - 1, decltype(subrange)>(std::forward<decltype(subrange)>(subrange), m_strides.next());
+		}
+		std::array<size_t, NDIMS> shape()
+		{
+			return m_strides.getShape(size());
 		}
 
 	private:
@@ -1353,55 +1468,67 @@ namespace sci
 			RANGE m_range;
 		};
 		std::shared_ptr<data_members_t> m_dataMembers;
+		const GridPremultipliedStridesReference<NDIMS> m_strides;
 	};
 
-	template < class RANGE>
+
+	template <size_t NDIMS, class RANGE>
 	requires std::ranges::random_access_range<RANGE>
-	grid_view(RANGE&&)->grid_view<RANGE>;
+		grid_view(RANGE&&, const GridPremultipliedStridesReference<NDIMS>&)->grid_view<NDIMS, RANGE>;
+
+	template <size_t NDIMS, class RANGE>
+	requires std::ranges::random_access_range<RANGE>
+		grid_view(RANGE&&)->grid_view<1, RANGE>;
 
 	
 
-	static_assert((bool)std::ranges::random_access_range<grid_view<std::deque<double>>>, "sci::grid_view failed the test for being a random access range");
-	static_assert((bool)std::ranges::range<grid_view<std::deque<double>>>, "sci::grid_view failed the test for being a range");
-	static_assert((bool)std::random_access_iterator<std::ranges::iterator_t<grid_view<std::deque<double>>>>, "sci::grid_view failed the test for having a random access iterator");
-	static_assert((bool)std::bidirectional_iterator<std::ranges::iterator_t<grid_view<std::deque<double>>>>, "sci::grid_view failed the test for having a bidirectional iterator");
-	static_assert(std::input_iterator<std::ranges::iterator_t<grid_view<std::deque<double>>>>, "sci::grid_view failed the test for having a input iterator");
-	static_assert(std::forward_iterator<std::ranges::iterator_t<grid_view<std::deque<double>>>>, "sci::grid_view failed the test for having a forward iterator");
-	static_assert(std::output_iterator<std::ranges::iterator_t<grid_view<std::deque<double>>>, int>, "sci::grid_view failed the test for having a output iterator");
-	static_assert(std::indirectly_readable<std::ranges::iterator_t<grid_view<std::deque<double>>>>, "sci::grid_view failed the test for having a indirectly readable iterator");
-	static_assert((bool)!std::ranges::contiguous_range< grid_view<std::deque<double>>>, "grid_view<std::deque<>> should not be contiguous");
+	static_assert((bool)std::ranges::random_access_range<grid_view<1, std::deque<double>>>, "sci::grid_view failed the test for being a random access range");
+	static_assert((bool)std::ranges::range<grid_view<1, std::deque<double>>>, "sci::grid_view failed the test for being a range");
+	static_assert((bool)std::random_access_iterator<std::ranges::iterator_t<grid_view<1, std::deque<double>>>>, "sci::grid_view failed the test for having a random access iterator");
+	static_assert((bool)std::bidirectional_iterator<std::ranges::iterator_t<grid_view<1, std::deque<double>>>>, "sci::grid_view failed the test for having a bidirectional iterator");
+	static_assert(std::input_iterator<std::ranges::iterator_t<grid_view<1, std::deque<double>>>>, "sci::grid_view failed the test for having a input iterator");
+	static_assert(std::forward_iterator<std::ranges::iterator_t<grid_view<1, std::deque<double>>>>, "sci::grid_view failed the test for having a forward iterator");
+	static_assert(std::output_iterator<std::ranges::iterator_t<grid_view<1, std::deque<double>>>, int>, "sci::grid_view failed the test for having a output iterator");
+	static_assert(std::indirectly_readable<std::ranges::iterator_t<grid_view<1, std::deque<double>>>>, "sci::grid_view failed the test for having a indirectly readable iterator");
+	static_assert((bool)!std::ranges::contiguous_range< grid_view<1, std::deque<double>>>, "grid_view<std::deque<>> should not be contiguous");
 
-	static_assert((bool)std::ranges::contiguous_range<grid_view<std::vector<double>>>, "sci::grid_view<std::vector<>> failed the test for being a contiguous range");
-	static_assert((bool)std::ranges::contiguous_range<std::vector<double>>, "std::vector<> failed the test for being a contiguous range");
+	static_assert((bool)std::ranges::contiguous_range<grid_view<1, std::vector<double>>>, "sci::grid_view<std::vector<>> failed the test for being a contiguous range");
 
-
+	template<size_t NDIMS>
 	struct grid_fn
 	{
+		grid_fn() = default;
+		grid_fn(const GridPremultipliedStridesReference<NDIMS>& strides)
+			:m_strides(strides)
+		{}
+		template <size_t NDIMS, class RANGE>
+		requires std::ranges::random_access_range<RANGE>
+		auto operator()(RANGE&& range, const GridPremultipliedStridesReference<NDIMS>& strides) const
+		{
+			return grid_view<NDIMS, RANGE>{ std::forward<RANGE>(range), m_strides };
+		}
+		template <size_t NDIMS>
+			auto operator()(const GridPremultipliedStridesReference<NDIMS>& strides) const
+		{
+			//This operator() returns a grid_fn which uses strides. This in turn can get piped
+			return grid_fn( strides );
+		}
 		template <typename RANGE>
 		requires std::ranges::random_access_range<RANGE>
-		auto operator()(RANGE&& range) const
+			friend auto operator|(RANGE&& range, grid_fn const& fn)
 		{
-			return grid_view{ std::forward<RANGE>(range) };
+			return grid_view<NDIMS, RANGE>{ std::forward<RANGE>(range), fn.m_strides };
 		}
-
-		template <typename RANGE>
-		requires std::ranges::random_access_range<RANGE>
-		friend auto operator|(RANGE&& range, grid_fn const&)
-		{
-			return grid_view{ std::forward<RANGE>(range) };
-		}
-
+	private:
+		const GridPremultipliedStridesReference<NDIMS> m_strides;
 	};
 
 	namespace views
 	{
-		grid_fn constexpr grid;
+		template<size_t NDIMS>
+		grid_fn<NDIMS> grid;
 	}
 
-
-
-	std::deque<double> que{ 1., 2., 3., 4., 5., 6. };
-	auto gridRange = que | views::grid;
 
 
 
