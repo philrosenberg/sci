e70e9c672e96261d64c1e1a90c2822514b11ed06
Phil Rosenberg <p.d.rosenberg@gmail.com>
2025-05-30 21:57:23 +0100

    Added som efilter algorithms

--- include/svector/algorithm.h
+++ include/svector/algorithm.h
@@ -2,6 +2,7 @@
 #define SCI_ALGORITHM_H
 #include<algorithm>
 #include<iterator>
+#include<svector/math.h>
 namespace sci
 {
 	//This sci vesion of std::transform avoids the MSVC security warnings which won't switch off
@@ -23,8 +24,22 @@ namespace sci
 	}
 #endif
 
-	template<class InputIt, class FilterIt, class OutputIt, class UnaryPred>
-	OutputIt copy_if(InputIt first, InputIt last, FilterIt filter_first, OutputIt dest_first, UnaryPred pred)
+	template< class T>
+	bool default_pred(const T& t)
+	{
+		return bool(t);
+	}
+
+	template<class T, class U>
+	concept Unary_Predicate = requires(T & t, U & u)
+	{
+		{ t(u) }-> std::convertible_to<bool>;
+	};
+
+	//differs from std::copy_if by having the if based on a potentially other container
+	//also has defined behaviour if input and dest overlap providing dest < input
+	template<class InputIt, class FilterIt, class OutputIt, Unary_Predicate<decltype(*FilterIt())> UnaryPred = decltype(sci::default_pred<decltype(*FilterIt())>)>
+	OutputIt copy_if(InputIt first, InputIt last, FilterIt filter_first, OutputIt dest_first, UnaryPred pred = sci::default_pred<decltype(*filter_first)>)
 	{
 		for (; first != last; ++first, ++filter_first)
 		{
@@ -37,10 +52,76 @@ namespace sci
 		return dest_first;
 	}
 
-	template<std::ranges::forward_range T, std::ranges::forward_range U, std::ranges::forward_range V, class UnaryPred>
-	auto copy_if(const T &input, const U &filter, V &dest, UnaryPred pred)
+	//differs from std::copy_if by having the if based on a potentially other container
+	template<std::ranges::forward_range T, std::ranges::forward_range U, std::ranges::forward_range V, Unary_Predicate<typename U::value_type> UnaryPred = decltype(sci::default_pred<typename U::value_type>)>
+	auto copy_if(const T &input, const U &filter, V &dest, UnaryPred pred = sci::default_pred<typename U::value_type>)
 	{
 		return copy_if(input.begin(), input.end(), filter.begin(), dest.begin(), pred);
+	}
+
+	//differs from std::remove_if by having the if based on a potentially other container
+	template<class InputIt, class FilterIt, Unary_Predicate<decltype(*FilterIt())> UnaryPred = decltype(sci::default_pred<decltype(*FilterIt())>)>
+	InputIt remove_if(InputIt first, InputIt last, FilterIt filter_first, UnaryPred pred = sci::default_pred<decltype(*filter_first)>)
+	{
+		InputIt dest = first;
+		for (; first != last; ++first, ++filter_first)
+		{
+			if (!pred(*filter_first))
+			{
+				*dest = std::move(*first);
+				++dest;
+			}
+		}
+		return dest;
+	}
+
+	//differs from std::remove_if by having the if based on a potentially other container
+	template<std::ranges::forward_range T, std::ranges::forward_range U, Unary_Predicate<typename U::value_type> UnaryPred = decltype(sci::default_pred<typename U::value_type>)>
+	auto remove_if(const T& input, const U& filter, UnaryPred pred = sci::default_pred<typename U::value_type>)
+	{
+		return remove_if(input.begin(), input.end(), filter.begin(), pred);
+	}
+
+	template<class InputIt, class FilterIt, Unary_Predicate<decltype(*FilterIt())> UnaryPred = decltype(sci::default_pred<decltype(*FilterIt())>)>
+	InputIt keep_if(InputIt first, InputIt last, FilterIt filter_first, UnaryPred pred = sci::default_pred<decltype(*filter_first)>)
+	{
+		InputIt dest = first;
+		for (; first != last; ++first, ++filter_first)
+		{
+			if (pred(*filter_first))
+			{
+				*dest = std::move(*first);
+				++dest;
+			}
+		}
+		return dest;
+	}
+
+	template<std::ranges::forward_range T, std::ranges::forward_range U, Unary_Predicate<typename U::value_type> UnaryPred = decltype(sci::default_pred<typename U::value_type>)>
+	auto keep_if(const T& input, const U& filter, UnaryPred pred = sci::default_pred<typename U::value_type>)
+	{
+		return keep_if(input.begin(), input.end(), filter.begin(), pred);
+	}
+
+	template<class InputIt, Unary_Predicate<decltype(*InputIt())> UnaryPred = decltype(sci::default_pred<decltype(*InputIt())>)>
+	InputIt keep_if(InputIt first, InputIt last, UnaryPred pred = sci::default_pred<decltype(*first)>)
+	{
+		InputIt dest = first;
+		for (; first != last; ++first)
+		{
+			if (pred(*first))
+			{
+				*dest = std::move(*first);
+				++dest;
+			}
+		}
+		return dest;
+	}
+
+	template<std::ranges::forward_range T, std::ranges::forward_range V, class UnaryPred = decltype(sci::default_pred<typename T::value_type>)>
+	auto keep_if(const T& input, UnaryPred pred = sci::default_pred<typename T::value_type>)
+	{
+		return keep_if(input.begin(), input.end(), pred);
 	}
 
 	template<class InputIt, class IndexIt, class OutputIt>
