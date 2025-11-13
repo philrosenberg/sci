bed0facc0640bd50a1a71809627d07fee73a8933
Phil Rosenberg <p.d.rosenberg@gmail.com>
2025-11-13 12:00:05 +0000

    Removed the Bars class
    I realised the bars class was actualy not needed and the classes
    which inherited from Bars could just directly inherit from Data.

--- include/svector/dep/plot/errorBar.h
+++ include/svector/dep/plot/errorBar.h
@@ -8,15 +8,16 @@ namespace sci
 {
 	namespace plot
 	{
-		class HorizontalErrorBars : public Bars
+		class HorizontalErrorBars : public Data<std::vector<double>, std::vector<double>, std::vector<double>, std::vector<double>>
 		{
 		public:
 			HorizontalErrorBars(std::span<const double> xs, std::span<const double> ys, std::span<const double> plusErrors, std::span<const double> minusErrors, std::shared_ptr<Axis> xAxis, std::shared_ptr<Axis> yAxis, Length stopLength, const LineStyle style = sci::plot::LineStyle(), bool useForAutoscale = true, std::shared_ptr<splotTransformer> transformer = nullptr)
 				: PlotableItem(xAxis, yAxis, transformer),
-				Bars(xs, ys, plusErrors, minusErrors, xAxis, yAxis, useForAutoscale, transformer)
+				Data<std::vector<double>, std::vector<double>, std::vector<double>, std::vector<double>>({ xAxis, yAxis, xAxis, xAxis }, transformer, xs, ys, (xs | sci::views::grid<1>) + (plusErrors | sci::views::grid<1>), (xs | sci::views::grid<1>) - (minusErrors | sci::views::grid<1>))
 			{
 				m_style = style;
 				m_stopLength = stopLength;
+				m_useForAutoscale = useForAutoscale;
 			}
 			void plotData(Renderer& renderer, grPerMillimetre scale) const override
 			{
@@ -26,10 +27,10 @@ namespace sci
 				renderer.setPen(m_style.getColour(), m_style.getWidth(), m_style.getPattern());
 
 				//note that we render in two halves so that if any of the error bars are negative we stil hit the x, y point
-				const std::vector<double>& xs = getAlongData();
-				const std::vector<double>& ys = getAcrossData();
-				const std::vector<double>& minXs = getMinAlongData();
-				const std::vector<double>& maxXs = getMaxAlongData();
+				const std::vector<double>& xs = getData<0>();
+				const std::vector<double>& ys = getData<1>();
+				const std::vector<double>& minXs = getData<2>();
+				const std::vector<double>& maxXs = getData<3>();
 				for (size_t i = 0; i < getNPoints(); ++i)
 				{
 					Point pCentre = getPointFromLoggedIfNeededData(xs[i], ys[i]);
@@ -42,21 +43,28 @@ namespace sci
 					renderer.line(p2 - halfStop, p2 + halfStop);
 				}
 			}
+			virtual void autoscaleAxes() override
+			{
+				if (!m_useForAutoscale)
+					return;
+				Data<std::vector<double>, std::vector<double>, std::vector<double>, std::vector<double>>::autoscaleAxes();
+			}
 		private:
 			LineStyle m_style;
 			Length m_stopLength;
 			bool m_useForAutoscale;
 		};
 
-		class VerticalErrorBars : public Bars
+		class VerticalErrorBars : public Data<std::vector<double>, std::vector<double>, std::vector<double>, std::vector<double>>
 		{
 		public:
 			VerticalErrorBars(std::span<const double> xs, std::span<const double> ys, std::span<const double> plusErrors, std::span<const double> minusErrors, std::shared_ptr<Axis> xAxis, std::shared_ptr<Axis> yAxis, Length stopLength, const LineStyle style = sci::plot::LineStyle(), bool useForAutoscale = true, std::shared_ptr<splotTransformer> transformer = nullptr)
 				: PlotableItem(xAxis, yAxis, transformer),
-				Bars(ys, xs, plusErrors, minusErrors, yAxis, xAxis, useForAutoscale, transformer)
+				Data<std::vector<double>, std::vector<double>, std::vector<double>, std::vector<double>>({ xAxis, yAxis, yAxis, yAxis }, transformer, xs, ys, (ys | sci::views::grid<1>) + (plusErrors | sci::views::grid<1>), (ys | sci::views::grid<1>) - (minusErrors | sci::views::grid<1>))
 			{
 				m_style = style;
 				m_stopLength = stopLength;
+				m_useForAutoscale = useForAutoscale;
 			}
 
 			void plotData(Renderer& renderer, grPerMillimetre scale) const override
@@ -66,10 +74,10 @@ namespace sci
 
 				renderer.setPen(m_style.getColour(), m_style.getWidth(), m_style.getPattern());
 
-				const std::vector<double>& xs = getAcrossData();
-				const std::vector<double>& ys = getAlongData();
-				const std::vector<double>& minYs = getMinAlongData();
-				const std::vector<double>& maxYs = getMaxAlongData();
+				const std::vector<double>& xs = getData<0>();
+				const std::vector<double>& ys = getData<1>();
+				const std::vector<double>& minYs = getData<2>();
+				const std::vector<double>& maxYs = getData<3>();
 
 				for (size_t i = 0; i < getNPoints(); ++i)
 				{
@@ -86,6 +94,7 @@ namespace sci
 		private:
 			LineStyle m_style;
 			Length m_stopLength;
+			bool m_useForAutoscale;
 		};
 	}
 }
--- include/svector/dep/plot/fillBars.h
+++ include/svector/dep/plot/fillBars.h
@@ -8,12 +8,12 @@ namespace sci
 {
 	namespace plot
 	{
-		class VerticalBars : public Bars
+		class VerticalBars : public Data<std::vector<double>, std::vector<double>, std::vector<double>>
 		{
 		public:
 			VerticalBars(std::span<const double> xs, std::span<const double> ys, std::span<const double> widths, std::shared_ptr<Axis> xAxis, std::shared_ptr<Axis> yAxis, const LineStyle& lineStyle, const FillStyle& fillStyle, double zeroLine = 0.0, std::shared_ptr<splotTransformer> transformer = nullptr)
 				: PlotableItem(xAxis, yAxis, transformer), m_yAxis(yAxis),
-				Bars(xs, ys, half(widths), half(widths), xAxis, yAxis, true, transformer)
+				Data<std::vector<double>, std::vector<double>, std::vector<double>>({ xAxis, xAxis, yAxis }, transformer, (xs | sci::views::grid<1>) - 0.5 * (widths | sci::views::grid<1>), (xs | sci::views::grid<1>) + 0.5 * (widths | sci::views::grid<1>), ys)
 			{
 				//a note on the above - the result of xs-0.5*widths and xs+0.5*widths is an r-value, meaning we
 				//can't directly take it's address. However, when we assign it to a const reference the temporary's
@@ -34,9 +34,9 @@ namespace sci
 
 				double zeroLine = m_yAxis->isLog() ? m_zeroLineLogged : m_zeroLineLinear;
 
-				const std::vector<double>& minXs = getMinAlongData();
-				const std::vector<double>& maxXs = getMaxAlongData();
-				const std::vector<double>& ys = getAcrossData();
+				const std::vector<double>& minXs = getData<0>();
+				const std::vector<double>& maxXs = getData<1>();
+				const std::vector<double>& ys = getData<2>();
 
 				for (size_t i = 0; i < getNPoints(); ++i)
 				{
@@ -48,16 +48,9 @@ namespace sci
 			virtual void autoscaleAxes() override
 			{
 				m_yAxis->expand(m_zeroLineLinear);
-				Bars::autoscaleAxes();
+				Data<std::vector<double>, std::vector<double>, std::vector<double>>::autoscaleAxes();
 			}
 		private:
-			static std::vector<double> half(const std::span<const double> values)
-			{
-				std::vector<double> result(values.begin(), values.end());
-				for (auto& r : result)
-					r = 0.5 * r;
-				return result;
-			}
 			FillStyle m_fillStyle;
 			LineStyle m_lineStyle;
 			double m_zeroLineLinear;
@@ -65,12 +58,12 @@ namespace sci
 			std::shared_ptr<Scale> m_yAxis;
 		};
 
-		class HorizontalBars : public Bars
+		class HorizontalBars : public Data<std::vector<double>, std::vector<double>, std::vector<double>>
 		{
 		public:
 			HorizontalBars(std::span<const double> xs, std::span<const double> ys, std::span<const double> widths, std::shared_ptr<Axis> xAxis, std::shared_ptr<Axis> yAxis, const LineStyle& lineStyle, const FillStyle& fillStyle, double zeroLine = 0.0, std::shared_ptr<splotTransformer> transformer = nullptr)
 				: PlotableItem(xAxis, yAxis, transformer), m_xAxis(xAxis),
-				Bars(ys, xs, half(widths), half(widths), yAxis, xAxis, true, transformer)
+				Data<std::vector<double>, std::vector<double>, std::vector<double>>({ xAxis, xAxis, yAxis }, transformer, xs, (ys | sci::views::grid<1>) - 0.5 * (widths | sci::views::grid<1>), (ys | sci::views::grid<1>) + 0.5 * (widths | sci::views::grid<1>))
 			{
 				//a note on the above - the result of xs-0.5*widths and xs+0.5*widths is an r-value, meaning we
 				//can't directly take it's address. However, when we assign it to a const reference the temporary's
@@ -91,9 +84,9 @@ namespace sci
 
 				double zeroLine = m_xAxis->isLog() ? m_zeroLineLogged : m_zeroLineLinear;
 
-				const std::vector<double>& minYs = getMinAlongData();
-				const std::vector<double>& maxYs = getMaxAlongData();
-				const std::vector<double>& xs = getAcrossData();
+				const std::vector<double>& xs = getData<0>();
+				const std::vector<double>& minYs = getData<1>();
+				const std::vector<double>& maxYs = getData<2>();
 
 				for (size_t i = 0; i < getNPoints(); ++i)
 				{
@@ -106,21 +99,55 @@ namespace sci
 			virtual void autoscaleAxes() override
 			{
 				m_xAxis->expand(m_zeroLineLinear);
-				Bars::autoscaleAxes();
+				Data<std::vector<double>, std::vector<double>, std::vector<double>>::autoscaleAxes();
 			}
 		private:
-			static std::vector<double> half(const std::span<const double> values)
-			{
-				std::vector<double> result(values.begin(), values.end());
-				for (auto& r : result)
-					r = 0.5 * r;
-				return result;
-			}
 			FillStyle m_fillStyle;
 			LineStyle m_lineStyle;
 			double m_zeroLineLinear;
 			double m_zeroLineLogged;
 			std::shared_ptr<Scale> m_xAxis;
+		};
+
+		class Boxes : public Data<std::vector<double>, std::vector<double>, std::vector<double>, std::vector<double>>
+		{
+		public:
+			Boxes(std::span<const double> x1s, std::span<const double> x2s, std::span<const double> y1s, std::span<const double> y2s, std::shared_ptr<Axis> xAxis, std::shared_ptr<Axis> yAxis, const LineStyle& lineStyle, const FillStyle& fillStyle, std::shared_ptr<splotTransformer> transformer = nullptr)
+				: PlotableItem(xAxis, yAxis, transformer),
+				Data<std::vector<double>, std::vector<double>, std::vector<double>, std::vector<double>>({ xAxis, xAxis, yAxis, yAxis }, transformer, x1s, x2s, y1s, y2s)
+			{
+				//a note on the above - the result of xs-0.5*widths and xs+0.5*widths is an r-value, meaning we
+				//can't directly take it's address. However, when we assign it to a const reference the temporary's
+				//lifetime is extended to the lifetime of the reference, so then we can legally get it's address. 
+				m_lineStyle = lineStyle;
+				m_fillStyle = fillStyle;
+			}
+
+			void plotData(Renderer& renderer, grPerMillimetre scale) const override
+			{
+				if (!hasData())
+					return;
+
+				renderer.setPen(m_lineStyle.getColour(), m_lineStyle.getWidth(), m_lineStyle.getPattern());
+				renderer.setBrush(m_fillStyle.getColour());
+
+
+				const std::vector<double>& x1s = getData<0>();
+				const std::vector<double>& x2s = getData<1>();
+				const std::vector<double>& y1s = getData<2>();
+				const std::vector<double>& y2s = getData<3>();
+
+				for (size_t i = 0; i < getNPoints(); ++i)
+				{
+					Point p1 = getPointFromLoggedIfNeededData(x1s[i], y1s[i]);
+					Point p2 = getPointFromLoggedIfNeededData(x2s[i], y2s[i]);
+					renderer.rectangle(p1, p2);
+				}
+			}
+
+		private:
+			FillStyle m_fillStyle;
+			LineStyle m_lineStyle;
 		};
 
 		class Fill : public Data<std::vector<double>, std::vector<double>>
